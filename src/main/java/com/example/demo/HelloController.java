64dea9fc13b9dd789ee2651b2c5a5dd8e8a78638
nihao <shenrinka1234@gmail.com>
2026-06-17 13:03:02 +0800

    修正：移除重複定義的 home 方法

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,20 +1,12 @@
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class HelloController {
 
     private List<String> messages = new ArrayList<>();
 
-    // 當你直接打開網址時，會顯示這個首頁
+    // 確保這裡只有這一個 home() 方法
     @GetMapping("/")
     public String home() {
-        return "歡迎！請使用 /add?msg=內容 留言，或使用 /list 查看內容。";
+        return "歡迎！請使用 /add?msg=內容 留言，或使用 /list 查看。";
     }
 
     @GetMapping("/add")
@@ -26,9 +18,5 @@ public class HelloController {
     @GetMapping("/list")
     public String listMessages() {
         return "目前的留言板內容: " + messages.toString();
-    }
-    @GetMapping("/")
-    public String home() {
-        return "這是首頁！請使用 /add?msg=你的訊息 來留言，或使用 /list 查看。";
     }
 }
