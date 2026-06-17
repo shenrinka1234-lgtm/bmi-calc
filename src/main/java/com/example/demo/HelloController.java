49be8433c493a95da4a3610e9dbeadd5f81a690f
nihao <shenrinka1234@gmail.com>
2026-06-17 11:43:04 +0800

    增加首頁測試

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "BMI 計算器網站已經成功上線！";
+    }
+}
