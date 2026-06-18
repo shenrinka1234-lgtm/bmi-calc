be6849a40b57b493675490a40b9a2cabfa2e7925
celine <shenrinka1234@gmail.com>
2026-06-18 15:11:02 +0800

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -73,7 +73,7 @@ public class HelloController {
     }
 
  
-    @DeleteMapping("/health-logs/{id}")
+    @DeleteMapping("/{id}")
 public void deleteLog(@PathVariable Long id) {
     repository.deleteById(id);
 }
